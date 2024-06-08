a5cd7cad196b04975a58dd4cea42070b4e1f14c0
Ivo <game4life49@live.nl>
2024-06-08 12:35:13 +0200

    Added godoc comments

--- simple_calculations.go
+++ simple_calculations.go
@@ -1,5 +1,10 @@
+// Package simple_calculations provides some simple calculations
 package simple_calculcations
 
+// Add adds the two input integers and returns the result.
+// For more information, see [mathisfun].
+//
+// [mathisfun]: https://www.mathisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
