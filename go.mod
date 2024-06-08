1e04e75b789a29cddd196fc54abf8c4e08c1427c
Ivo <game4life49@live.nl>
2024-06-08 12:40:32 +0200

    Made the Add function generic

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/danoctum/simple_calculations
 
 go 1.21.4
+
+require golang.org/x/exp v0.0.0-20240604190554-fc45aab8b7f8 // indirect
--- simple_calculations.go
+++ simple_calculations.go
@@ -1,10 +1,16 @@
 // Package simple_calculations provides some simple calculations
 package simple_calculcations
 
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add adds the two input integers and returns the result.
 // For more information, see [mathisfun].
 //
 // [mathisfun]: https://www.mathisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
